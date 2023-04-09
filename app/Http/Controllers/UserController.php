027b98e3481080e31312e4016a07d02d8d803dc9
Craig Ben Cadag <craigbencadag@gmail.com>
2023-04-09 15:23:06 +0800

    add components

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,11 +3,25 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
 
 class UserController extends Controller
 {
-    public function UserDashboard(){
+    public function UserDashboard()
+    {
 
         return view('user.user_dashboard');
+    }
+
+    public function AdminLogout(Request $request)
+    {
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/login');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -38,6 +38,8 @@ Route::middleware(['auth', 'role:admin'])->group(function(){
 
     Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
 
+    Route::get('/admin/logout', [AdminController::class, 'AdminLogout'])->name('admin.logout');
+
 });
 
 //middleware for agent
